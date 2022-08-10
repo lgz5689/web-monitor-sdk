6ff6c6f2c223d564170d0da330d69123f4dea2a0
lgz <1026838160@qq.com>
2022-08-10 16:27:36 +0800

    feat: add handle error

---
+++ src/error/index.ts
@@ -0,0 +1,47 @@
+import { lazyReport } from '../utils/report'
+
+export const error = () => {
+  // handle resource error
+  window.addEventListener(
+    'error',
+    (e: any) => {
+      if (!e.target) return
+
+      lazyReport({
+        url: window.location.href,
+        type: 'error',
+        subType: 'resource',
+        resourceUrl: e.target.src || e.target.href,
+        resourceHtml: e.target.outerHTML,
+        resourceType: e.target.tagName,
+        startTime: performance.now(),
+      })
+    },
+    true
+  )
+
+  // handle unhandledrejection
+  window.addEventListener('unhandledrejection', (e) => {
+    lazyReport({
+      url: window.location.href,
+      type: 'error',
+      subType: 'promise',
+      reason: e.reason?.stack,
+      startTime: performance.now(),
+    })
+  })
+
+  // handle js error
+  window.onerror = (msg, url, line, column, error) => {
+    lazyReport({
+      url,
+      type: 'error',
+      subType: 'js',
+      msg,
+      line,
+      column,
+      error: error?.stack,
+      startTime: performance.now(),
+    })
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { setAppid } from './config'
+import { error } from './error/index'
 
 const app = {
   init(appid: string) {
     setAppid(appid)
+    error()
   },
 }
 
