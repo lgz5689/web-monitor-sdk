08f569fc0330cdef472f9502ea4ab3dea2077c8d
lgz <1026838160@qq.com>
2022-08-12 22:59:18 +0800

    feat: 新增用户点击监控

--- src/behavior/index.ts
+++ src/behavior/index.ts
@@ -1,7 +1,9 @@
+import { onClick } from './onClick'
 import { pageDuration } from './pageDuration'
 import { pageView } from './pageView'
 
 export default function behavior() {
   pageView()
   pageDuration()
+  onClick()
 }
---
+++ src/behavior/onClick.ts
@@ -0,0 +1,44 @@
+import { lazyReport } from '../utils/report'
+
+export const onClick = () => {
+  const url = window.location.href
+  const arr = ['mousedown', 'touchstart']
+
+  arr.forEach((item) => {
+    let time: NodeJS.Timeout
+    window.addEventListener(item, (event: any) => {
+      clearTimeout(time)
+
+      time = setTimeout(() => {
+        const target = event.target
+        const { top, left } = target?.getBoundingClientRect()
+
+        lazyReport({
+          type: 'behavior',
+          subType: 'click',
+          eventType: item,
+          pageURL: url,
+          pageHeight: document.documentElement.scrollHeight || document.body.scrollHeight,
+          scrollTop: document.documentElement.scrollTop || document.body.scrollTop,
+          tagName: target?.tagName,
+          paths: event.path?.map((item: any) => item.tagName).filter(Boolean),
+          startTime: event.timeStamp,
+          outerHTML: target?.outerHTML,
+          innerHTML: target?.innerHTML,
+          position: {
+            top,
+            left,
+          },
+          style: {
+            width: target?.offsetWidth,
+            height: target?.offsetHeight,
+          },
+          viewport: {
+            width: window.innerWidth,
+            height: window.innerHeight,
+          },
+        })
+      }, 500)
+    })
+  })
+}
