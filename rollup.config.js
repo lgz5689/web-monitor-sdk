935411bd6ed660eede67d6947a631a7bdcbd151a
lgz <1026838160@qq.com>
2022-08-14 21:35:59 +0800

    build: 打包支持esm和cjs

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,27 @@ export default [
     output: {
       file: 'dist/monitor.js',
       format: 'iife',
-      name: '__monitor',
+      name: '_monitor',
+      sourcemap: true,
+    },
+  },
+  {
+    plugins,
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/monitor.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  },
+  {
+    plugins,
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/monitor.cjs.js',
+      format: 'cjs',
+      name: '_monitor',
+      sourcemap: true,
     },
   },
 ]
